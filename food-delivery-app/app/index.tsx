1fb154811c4ab17385f4fb63e02999eb0be99dcc
Madhav Agarwal1411 <agarwalmadhav1411@gmail.com>
2025-04-09 16:54:48 +0530

    custom button component updated
    Co-Authored-By: Mukund Singh Bisht <155366439+MukundSB19@users.noreply.github.com>

--- food-delivery-app/app/index.tsx
+++ food-delivery-app/app/index.tsx
@@ -7,7 +7,7 @@ import { Redirect } from 'expo-router';
 
 const index = () => {
   return (
-  <Redirect href={"/(auth)/Welcome"} />  
+  <Redirect href={"/(auth)/welcome"} />  
   )
 }
 
--- food-delivery-app/components/CustomButton.tsx
+++ food-delivery-app/components/CustomButton.tsx
@@ -1,27 +1,36 @@
-import React from "react";
+// import { ButtonProps } from "@/types/types";
+import { ButtonProps } from "@/types/types";
 import { TouchableOpacity, Text } from "react-native";
 
 const CustomButton = ({
-  title,
-  onPress,
-  disabled = false,
-  className = "",
-  textClass = "",
-}) => {
-  return (
-    <TouchableOpacity
-      className={`bg-buttonPrimary-0 py-3 px-5 w-4/5 rounded-full items-center shadow-md shadow-black ${
-        disabled ? "bg-gray-400" : ""
-      } ${className}`}
-      onPress={onPress}
-      disabled={disabled}
-      activeOpacity={0.8}
-    >
-      <Text className={`text-white font-regularFont tracking-tighter text-3xl ${textClass}`}>
-        {title}
-      </Text>
-    </TouchableOpacity>
-  );
+    title,
+    disabled = false,
+    className,
+      bgVariant,
+      textVariant,
+      textStyle,
+      IconLeft,
+      IconRight,
+      ...props
+}: ButtonProps) => {
+    return (
+        <TouchableOpacity
+            className={`bg-buttonPrimary-0 py-3 px-5 w-4/5 rounded-full items-center shadow-md shadow-black ${
+                disabled ? "bg-gray-400" : ""
+            } ${className}`}
+            disabled={disabled}
+            activeOpacity={0.8}
+            {...props}
+        >
+            {IconLeft && <IconLeft />}
+            <Text
+                className={`text-white font-regularFont tracking-tighter text-3xl ${textStyle}`}
+            >
+                {title}
+            </Text>
+            {IconRight && <IconRight />}
+        </TouchableOpacity>
+    );
 };
 
 export default CustomButton;
