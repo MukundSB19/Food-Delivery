4f069a5326481a3c0c198f25ede8dd8d59a6cc72
Mukund Singh Bisht <Mukundsingh05@outlook.com>
2025-06-07 17:44:36 +0530

    Auth v2
    Co-Authored-By: Madhav Agarwal <agarwalmadhav1411@gmail.com>

--- server/src/controllers/auth/auth.js
+++ server/src/controllers/auth/auth.js
@@ -4,13 +4,13 @@ import jwt from "jsonwebtoken";
 const generateTokens = (user) => {
   const accessToken = jwt.sign(
     { userId: user._id, role: user.role },
-    process.env.Access_TOKEN_SECRET,
+    process.env.ACCESS_TOKEN_SECRET,
     { expiresIn: "1d" }
   );
 
   const refreshToken = jwt.sign(
     { userId: user._id, role: user.role },
-    process.env.Access_TOKEN_SECRET,
+    process.env.REFRESH_TOKEN_SECRET,
     { expiresIn: "1d" }
   );
   return { accessToken, refreshToken };
@@ -71,3 +71,66 @@ export const loginDeliveryPartner = async(req,reply) => {
         return reply.status(500).send({message:"An error Occurred" ,error});
     }
 };
+
+ export const refreshToken = async(req,reply) => {
+  const {refreshToken}=req.body
+
+  if(!refreshToken){
+    return reply.status(401).send({
+      message: "Refresh token required"
+    });
+  };
+
+try {
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+  let user;
+
+if (decoded.role === "Customer"){
+user = await Customer.findById(decoded.userId);
+}else if (decoded.role === "DeliveryPartner"){
+  user = await DeliveryPartner.findById(decoded.userId);
+} else {
+  return reply.status(403).send({ message:"Invalid Role" });
+}
+if (!user) {
+  return reply.status(403).send({ message:"User not Found"});
+}
+const { accessToken, refreshToken: newRefreshToken } = generateTokens(user);
+
+return reply.send({
+  message:"Token Refreshed",
+  accessToken,
+  refreshToken: newRefreshToken,
+})
+
+} 
+catch(error){
+  return reply.status(403).send ({message:"Inavlid Refresh Token"});
+}
+}
+
+export const fetchUser = async (req,reply)=>{
+  try{
+    const {userId, role} = req.user;
+    let user;
+
+    if(role === "Customer"){
+      user = await Customer.findById(userId);
+    } else if (role === "DeliveryPartner"){
+      user = await DeliveryPartner.findById(userId);
+    } else {
+      return reply.status(403).send({ message: "Invalid Role"});
+    }
+    if (!user){
+      return reply.status(404).send({message:"User not found"});
+    }
+
+    return reply.send({
+      message:"User Fetched Succesfully",
+      user,
+    })
+  } catch (error){
+    return reply.status(500).send({ message:"an error occured" , error});
+  }
+}
+
