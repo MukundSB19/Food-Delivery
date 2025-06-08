d531300394eb19cedd1ef18d1a62d10744205bb3
Madhav Agarwal <agarwalmadhav1411@gmail.com>
2025-06-08 09:42:17 +0530

    controllers logic completed
    Co-Authored-By: Mukund Singh Bisht <155366439+MukundSB19@users.noreply.github.com>

--- server/src/controllers/order/order.js
+++ server/src/controllers/order/order.js
@@ -53,33 +53,107 @@ export const confirmOrder = async (req, reply) => {
 
     const deliveryPerson = await DeliveryPartner.findById(userId);
     if (!deliveryPerson) {
-        return reply.status(404).send({ message: "Delivery Person not found" });
-
+      return reply.status(404).send({ message: "Delivery Person not found" });
     }
     const order = await Order.findById(orderId);
-    if(!order) return reply.status(404).send({ message: "Order not found" });
+    if (!order) return reply.status(404).send({ message: "Order not found" });
 
-    if(order.status !== "available" ) {
-        return reply.status(400).send({ message: "Order is not available" });
+    if (order.status !== "available") {
+      return reply.status(400).send({ message: "Order is not available" });
     }
 
     order.status = "confirmed";
 
     order.deliveryPartner = userId;
     order.deliveryPersonLocation = {
-        latitude: deliveryPersonLocation?.latitude,
-        longitude: deliveryPersonLocation?.longitude,
-        address: deliveryPersonLocation.address || "",
+      latitude: deliveryPersonLocation?.latitude,
+      longitude: deliveryPersonLocation?.longitude,
+      address: deliveryPersonLocation.address || "",
     };
 
     req.server.io.to(orderId).emit("orderConfirmed", order);
     await order.save();
 
     return reply.send(order);
-
   } catch (error) {
     return reply
       .status(500)
       .send({ message: "Failed to confirm Order", error });
+  }
+};
+
+export const updateOrderStatus = async (req, reply) => {
+  try {
+    const { orderId } = req.params;
+    const { status, deliveryPersonLocation } = req.body;
+    const { userId } = req.user;
+
+    const deliveryPerson = await DeliveryPartner.findById(userId);
+    if (!deliveryPerson) {
+      return reply.status(404).send({ message: "Delivery person not found" });
+    }
+
+    const order = await Order.findById(orderId);
+
+    if (!order) return reply.status(404).send({ message: "Order not found" });
+
+    if (order.deliveryPartner.toString() !== userId) {
+      return reply.status(403).send({ message: "Unauthorized" });
+    }
+
+    order.status = status;
+    order.deliveryPersonLocation = deliveryPersonLocation;
+    await order.save();
+
+    req.server.io(orderId.emit("liveTrackingUpdates", order));
+
+    return reply.send(order);
+  } catch (error) {
+    return reply
+      .status(500)
+      .send({ message: "Failed to update order status", error });
+  }
+};
+
+export const getOrders = async (req, reply) => {
+  try {
+    const { status, consumerId, deliveryPartnerId, branchId } = req.query;
+    let query = {};
+
+    if (status) query.status = status;
+    if (customerId) query.customer = customerId;
+    if (deliveryPartnerId) {
+      query.delivveryPartner = deliveryPartnerId;
+      query.brach = branchId;
+    }
+
+    const orders = await Order.find(query).populate(
+      "customer branch items.item delivveryPartner"
+    );
+
+    return reply.send(orders);
+  } catch (error) {
+    return reply
+      .status(500)
+      .send({ message: "Failed to retrieve orders", error });
+  }
+};
+
+export const getOrderById = async (req, reply) => {
+  try {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId).populate(
+      "customer branch items.item deliveryPartner"
+    );
+    if(!order) {
+      return reply.status(404).send({ message: "Order not found" });
+    }
+
+    return reply.send(order);
+  } catch (error) {
+    return reply
+      .status(500)
+      .send({ message: "Failed to retrieve order", error });
   }
 };
---
+++ server/src/routes/index.js
@@ -0,0 +1,12 @@
+import { authRoutes } from "./auth.js"
+import { orderRoutes } from "./order.js"
+import { categoryRoutes, productRoutes } from "./product.js"
+
+const prefix = "/api/v1";
+
+export const registerRoute = async (fastify) => {
+    fastify.register(authRoutes, { prefix: prefix })
+    fastify.register(productRoutes, { prefix: prefix })
+    fastify.register(categoryRoutes, { prefix: prefix })
+    fastify.register(orderRoutes, { prefix: prefix })
+}
